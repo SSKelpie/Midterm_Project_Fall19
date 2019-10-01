29be44e4d3f4706ffb24489e56329f8a740fe6ba
Gabrielle Maia Dias <gmd371@nyu.edu>
2019-10-01 17:12:51 -0400

    Day 1: Got them primitive shapes up and the player walkin

---
+++ Midterm_Project_Proto/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movespeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) == (Input.GetKey(KeyCode.UpArrow)))
+        {
+            this.transform.Translate(new Vector3(0,0,movespeed* Time.deltaTime));
+        }
+        if(Input.GetKey (KeyCode.S) == (Input.GetKey(KeyCode.DownArrow)))
+        {
+            this.transform.Translate(new Vector3(0,0,-movespeed* Time.deltaTime));
+        }
+        if (Input.GetKey (KeyCode.D) == (Input.GetKey(KeyCode.RightArrow))){
+            this.transform.Translate(new Vector3(movespeed* Time.deltaTime,0,0));
+        }
+        if (Input.GetKey (KeyCode.A) == (Input.GetKey(KeyCode.LeftArrow))){
+            this.transform.Translate(new Vector3(-movespeed* Time.deltaTime,0,0));
+        }
+    }
+
+}
+
+
+
+
